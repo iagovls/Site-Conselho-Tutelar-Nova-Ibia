1b83655a22149e6f11dcdbc300c22c9845058593
Iago Vitor <iagovls@gmail.com>
2025-06-23 05:31:14 -0300

    Update next.config.mjs

--- site-conselho-tutelar/next.config.mjs
+++ site-conselho-tutelar/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/Site-Conselho-Tutelar-Nova-Ibia',
+    images: {
+        unoptimized: true
+    }
+};
 
 export default nextConfig;
